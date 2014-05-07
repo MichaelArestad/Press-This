af62dd7117ccf53d6f978cad7c5ca6c78e20c257
stephdau <stephane+github@automattic.com>
2014-05-07 15:22:33 -0400

    Let's make this official, shall we? :)
    WordPress plugin loader and readme.txt skeleton files.
    
    Note: http://wordpress.org/extend/plugins/press-this/ not requested yet
    but available.

---
+++ press-this.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Press This
+Plugin URI: http://wordpress.org/extend/plugins/press-this/
+Description: Posting images, links, and cat gifs will never be the same.
+Version: 0.1
+Author: Press This Team
+Author URI: https://corepressthis.wordpress.com/
+Text Domain: press-this
+Domain Path: /languages
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
