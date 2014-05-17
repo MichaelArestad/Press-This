ff0297ecbb51e82246d7f330cdf1b6d39cb1fec9
George Stephanis <georgestephanis@automattic.com>
2014-05-17 13:09:44 -0400

    Tidy up the include to use native header/footer.
    Will probably need to do something further to remove the left side menu.

--- wp-admin-press-this.php
+++ wp-admin-press-this.php
@@ -1,2 +1,14 @@
+<?php
+
+if ( ! current_user_can( 'edit_posts' ) || ! current_user_can( get_post_type_object( 'post' )->cap->create_posts ) ) {
+	wp_die( __( 'Cheatin&#8217; uh?' ) );
+}
+
+require_once( ABSPATH . 'wp-admin/admin-header.php' );
+?>
+
 <h1>WE DUCK PUNCHED IT</h1>
 <img src="https://i.cloudup.com/_abhsAX8qH.png" />
+
+<?php
+require_once( ABSPATH . 'wp-admin/admin-footer.php' );
