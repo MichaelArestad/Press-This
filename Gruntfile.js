f71effe6b27e4daa2738856f146f5bc1835d07e8
MichaelArestad <marestad@gmail.com>
2014-06-05 08:17:27 -0600

    SVG icons. 8)
    You’ll have to npm install again if you want to mess with them or the
    CSS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
+    // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
                 files: {
                     'css/press-this.css': 'scss/style.scss'
                 }
-            } 
+            }
         },
         autoprefixer: {
             options: {
@@ -44,6 +44,51 @@ module.exports = function(grunt) {
                 },
                 src: 'css/press-this.css'
             },
+        },
+        grunticon: {
+            dashicons: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/icons/',
+                    src: ['*.svg', '*.png'],
+                    dest: "images/icons/"
+                }],
+                options: {
+                    defaultWidth: '20px',
+                    defaultHeight: '20px'
+                }
+            }
+        },
+        svgmin: {
+            options: {
+                plugins: [
+                    { removeViewBox: false },
+                    { removeUselessStrokeAndFill: false }
+                ]
+            },
+            dist: {
+                files: [{               // Dictionary of files
+                    expand: true,       // Enable dynamic expansion.
+                    cwd: 'images/icons',     // Src matches are relative to this path.
+                    src: ['*.svg'],  // Actual pattern(s) to match.
+                    dest: 'images/icons',       // Destination path prefix.
+                    ext: '.svg'     // Dest filepaths will have this extension.
+                    // ie: optimise img/src/branding/logo.svg and store it in img/branding/logo.min.svg
+                }]
+            }
+        },
+        svgstore: {
+            defaults: {
+                options: {
+                    prefix : 'dashicons-', // This will prefix each ID
+                    svg: { // will be added as attributes to the resulting SVG
+                        viewBox : '0 0 20 20'
+                    }
+                },
+                files: {
+                    'images/icons/icons.svg': ['images/icons/*.svg']
+                },
+            }
         },
         watch: {
             // options: {
@@ -70,6 +115,13 @@ module.exports = function(grunt) {
                     livereload: true,
                     spawn: false,
                 }
+            },
+            svg: {
+                files: ['images/icons/*.svg'],
+                tasks: ['svgmin', 'svgstore', 'grunticon'],
+                options: {
+                    spawn: false,
+                }
             }
         }
     });
@@ -80,6 +132,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');      // watch files for changes
     grunt.loadNpmTasks('grunt-contrib-sass');       // Gettin Sassy!
     grunt.loadNpmTasks('grunt-autoprefixer');       // Auto-freaking-prefixer!!!
+    grunt.loadNpmTasks('grunt-grunticon');          // Grunticon!
+    grunt.loadNpmTasks( 'grunt-svgmin' );           // SVG minifier
+    grunt.loadNpmTasks('grunt-svgstore');           // SVG combiner
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['watch']);
