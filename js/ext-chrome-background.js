6deb54c81b09d84d3004706ebe2e381b72b7f848
stephdau <stephane+github@automattic.com>
2014-06-09 13:44:43 -0400

    Committing some temp exp code

--- js/ext-chrome-background.js
+++ js/ext-chrome-background.js
@@ -1,1 +1,13 @@
-alert('Booyah!');
+// experimental code, not functional yet
+chrome.browserAction.onClicked.addListener( function() {
+	chrome.tabs.getSelected( null, function ( tab ) {
+		var u = prompt('Please enter a WordPress install\'s URL', ''),
+			selected_text = '';
+		console.log('chrome.browserAction.onClicked');
+		if (null !== u && u.match(/^https?:\/\//)) {
+			u = u + '/wp-admin/press-this.php?u=' + encodeURI(tab.url);
+			console.log(chrome.windows.getgetCurrent(null, function(win){ return win; }));
+			WpPressThis_Bookmarklet( u, selected_text, chrome.windows.getgetCurrent(null, function(win){ return win; }) );
+		}
+	});
+});
