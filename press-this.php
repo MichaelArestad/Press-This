7db7b8ada5605ce6a6535a1d790df16ec607e0c7
MichaelArestad <marestad@gmail.com>
2014-06-09 09:39:43 -0600

    Added adminbar

--- press-this.php
+++ press-this.php
@@ -496,6 +496,8 @@ class WpPressThis {
 		$load_js_inc              = self::plugin_dir_url() . '/js/load.js';
 		$svg_icons_inc            = self::plugin_dir_path() . '/images/icons/icons.svg';
 		$form_action              = $runtime_url;
+		$site_title               = 'yoursite.com';
+		$site_url                 = '#';
 
 		// Echo HTML
 		echo <<<________HTMLDOC
@@ -520,6 +522,10 @@ ________HTMLDOC;
 			require_once( $svg_icons_inc );
 
 		echo <<<________HTMLDOC
+	<div class="adminbar">
+		<h1 class="current-site"><div href="#" class="dashicons dashicons-wordpress-alt"><svg class="icon"><use xlink:href="#dashicons-wordpress-alt" /></svg></div><a href="${site_url}">${site_title}</a></h1>
+		<a href="#" class="dashicons dashicons-admin-settings"><svg class="icon"><use xlink:href="#dashicons-admin-settings" /></svg>Settings</a>
+	</div>
 	<div id='wppt_app_container' class="editor">
 		<h2 id='wppt_title_container' class="post__title" contenteditable="true"></h2>
 		<div id='wppt_featured_image_container' class="featured-image-container">
