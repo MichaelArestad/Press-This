6ae91bc9efd3dcb1d77914d918105c5dd485d884
MichaelArestad <marestad@gmail.com>
2015-02-20 21:52:40 -0700

    added base IE8 styles

--- press-this.php
+++ press-this.php
@@ -645,7 +645,9 @@ class WP_Press_This {
 		}
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php echo get_option( 'blog_charset' ); ?>" />
 	<meta name="viewport" content="width=device-width">
