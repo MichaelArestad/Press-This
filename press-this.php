da49dd00a3accbf3c2d06bf6e4eb969cd487faa0
stephdau <stephane+github@automattic.com>
2015-02-23 14:36:02 -0500

    Version bump to prompt users to upgrade bookmarklet.
    New code added to bookmarklet in 129646c

--- press-this.php
+++ press-this.php
@@ -3,7 +3,7 @@
 Plugin Name: Press This
 Plugin URI: https://wordpress.org/plugins/press-this/
 Description: Posting images, links, and cat gifs will never be the same.
-Version: 0.0.8
+Version: 0.0.9
 Author: Press This Team
 Author URI: https://corepressthis.wordpress.com/
 Text Domain: press-this
